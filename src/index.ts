e33e01d86d0acd85732e220658a41514f4808535
LuoLuann <luo.luann@gmail.com>
2024-02-03 17:39:40 -0300

    feat: addded more configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from './server/Server'
+import 'dotenv/config'
 
-
-server.listen(5000, () => {
-    console.log("servidor rodando");
+server.listen(process.env.PORT, () => {
+    console.log(`servidor rodando ${process.env.PORT}`);
 });

07b98266a1726dfa339572a83281e48789bd9bb5
LuoLuann <luo.luann@gmail.com>
2024-02-03 20:43:47 -0300

    feat: add folders structure

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { server } from './server/Server'
 import 'dotenv/config'
 
 server.listen(process.env.PORT, () => {
-    console.log(`servidor rodando ${process.env.PORT}`);
+    console.log(`servidor rodando na porta: ${process.env.PORT}`);
 });
